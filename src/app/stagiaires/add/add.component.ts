df1f991cceb8e2ee969b1bec497ba348f0846906
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-01-03 13:30:40 +0100

    ouiouioui

--- src/app/stagiaires/add/add.component.ts
+++ src/app/stagiaires/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { StagiaireModel } from 'src/app/core/models/stagiaire-model';
 import { StagiaireService } from 'src/app/core/services/stagiaire-service';
 import { DateLessThan } from 'src/app/core/validators/date-less-than';
 
@@ -57,9 +58,11 @@ export class AddComponent implements OnInit {
 
   public onSubmit(): void {
     console.log(`Values to send : ${JSON.stringify(this.addStagiaireForm.value)}`);
-    this.stagiaireService.create(this.addStagiaireForm.value).subscribe(() => {
-      //this.router.navigate(['/', 'stagiaires']);
-      this.addStagiaireForm.reset();
+    this.stagiaireService.create(this.addStagiaireForm.value)
+        .subscribe((stagiaire: StagiaireModel) => {
+            console.log("Values received from backend", stagiaire);
+            this.router.navigate(['/', 'stagiaires']);
+            // this.addStagiaireForm.reset();
     });
   }
 }
