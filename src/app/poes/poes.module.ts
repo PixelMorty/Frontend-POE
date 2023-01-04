b4803f37783b61b336232280f5d097d066ac0d7e
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-01-04 11:21:07 +0100

    route added

---
+++ src/app/poes/poes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PoesRoutingModule } from './poes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PoesRoutingModule
+  ]
+})
+export class PoesModule { }
