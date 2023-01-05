ac214ca767660cbd21a171c9175d4d7e148a4b49
Celine <rat.celine@outlook.com>
2023-01-05 10:23:00 +0100

    Modification poe service et list component

--- src/app/poes/component/list/list.component.ts
+++ src/app/poes/component/list/list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Poe } from 'src/app/core/models/poe';
+import { PoeService } from '../../services/poe/poe.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  poes: Poe[] = [];
+  constructor(private _poeService: PoeService) {}
 
   ngOnInit(): void {
+    this._poeService.findAll().subscribe((poes: Poe[]) => {
+      this.poes = poes;
+    });
   }
-
 }
