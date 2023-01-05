5b6d5472b5f8addcb113dff8d31cbbe8829d6e58
Celine <rat.celine@outlook.com>
2023-01-05 16:51:07 +0100

    Add onDelete and getDisplayRowsNumber list poe

--- src/app/poes/component/list/list.component.ts
+++ src/app/poes/component/list/list.component.ts
@@ -1,4 +1,6 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Poe } from 'src/app/core/models/poe';
 import { PoeService } from '../../services/poe/poe.service';
 
@@ -10,11 +12,37 @@ import { PoeService } from '../../services/poe/poe.service';
 export class ListComponent implements OnInit {
   poes: Poe[] = [];
   public showLi: string = 'A';
-  constructor(private _poeService: PoeService) {}
+  constructor(private _poeService: PoeService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this._poeService.findAll().subscribe((poes: Poe[]) => {
       this.poes = poes;
+    });
+  }
+
+  public getDisplayRowsNumber(): number {
+    if (this.showLi === 'All') {
+      return this.poes.length;
+    }
+    let displayedItem: number = 0;
+    for (const poe of this.poes) {
+      if (poe.poeType === this.showLi) {
+        displayedItem += 1;
+      }
+    }
+    return displayedItem;
+  }
+
+  public onDelete(poe: Poe): void {
+    this._poeService.delete(poe).subscribe((response: HttpResponse<any>) => {
+      // Supprimer la ligne dans this.stagiaires
+      this.poes.splice(
+        this.poes.findIndex((obj: Poe) => obj.id === poe.id),
+        1
+      );
+      this.snackBar.open(`Le stagiaire ${poe.id} a été supprimé`, 'Compris', {
+        duration: 2500,
+      });
     });
   }
 }
