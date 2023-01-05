f22909acc13b6bfe7f28dd152be8ba1456a1b317
Lou Coutouly <lou.m.coutouly@gmail.com>
2023-01-05 16:55:09 +0100

    fixed icon

--- src/app/poes/poes.module.ts
+++ src/app/poes/poes.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PoesRoutingModule } from './poes-routing.module';
 import { ListComponent } from './component/list/list.component';
 import { ManageComponent } from './component/manage/manage.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ListComponent, ManageComponent],
-  imports: [CommonModule, PoesRoutingModule],
+  imports: [CommonModule, PoesRoutingModule, SharedModule],
 })
 export class PoesModule {}
