323893760529477c9f17354050c88486ad6f9a1d
Lou Coutouly <lou.m.coutouly@gmail.com>
2023-01-05 16:59:47 +0100

    "fix"

--- src/app/poes/component/list/list.component.ts
+++ src/app/poes/component/list/list.component.ts
@@ -9,13 +9,8 @@ import { PoeService } from '../../services/poe/poe.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
+
 export class ListComponent implements OnInit {
-onDelete(_t24: Poe) {
-throw new Error('Method not implemented.');
-}
-getDisplayRowsNumber() {
-throw new Error('Method not implemented.');
-}
   poes: Poe[] = [];
   public showLi: string = 'A';
   constructor(private _poeService: PoeService, private snackBar: MatSnackBar) {}
