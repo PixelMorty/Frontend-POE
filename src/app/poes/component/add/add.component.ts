d32eb00eb5bcf71ba12751cd370e30769b6de2cf
Lola Haverlan <haverlan.lola@hotmail.fr>
2023-01-06 16:28:39 +0100

    poes add component html ts

--- src/app/poes/component/add/add.component.ts
+++ src/app/poes/component/add/add.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Poe } from 'src/app/core/models/poe';
-import { StagiaireModel } from 'src/app/core/models/stagiaire-model';
-import { StagiaireService } from 'src/app/core/services/stagiaire-service';
-import { DateLessThan } from 'src/app/core/validators/date-less-than';
-import { PoesModule } from '../../poes.module';
 import { PoeService } from '../../services/poe/poe.service';
 
 @Component({
@@ -28,16 +24,10 @@ export class AddComponent implements OnInit {
 
   ngOnInit(): void {
     this.addPoeForm = this.formBuilder.group({
-      poeType: [
-        '', // Default value (here empty)
-        // <input placeholder="Helper text..." value="">
-        [
-          Validators.required // Indique que le contrôle doit impérativement être défini avec une valeur non nulle
-        ]
-      ],
+
       title: [
         '',
-        Validators.required
+        Validators.required,
       ],
 
       beginDate: [
@@ -52,14 +42,20 @@ export class AddComponent implements OnInit {
           Validators.required,
         ]
       ],
-
+      poeType: [
+        '', // Default value (here empty)
+        // <input placeholder="Helper text..." value="">
+        [
+          Validators.required // Indique que le contrôle doit impérativement être défini avec une valeur non nulle
+        ]
+      ],
     });
   }
 
 
   public onSubmit(): void {
     console.log(`Values to send : ${JSON.stringify(this.addPoeForm.value)}`);
-    this.addPoeForm.create(this.addPoeForm.value)
+    this.poeService.create(this.addPoeForm.value)
         .subscribe((poe: Poe) => {
             console.log("Values received from backend", poe);
             this.router.navigate(['/', 'poes']);
