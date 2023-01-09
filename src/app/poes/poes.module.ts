19af46920c07c2ab2df5a3f8a7e83da8cc7910a6
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-01-09 11:41:02 +0100

    merged2

--- src/app/poes/poes.module.ts
+++ src/app/poes/poes.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PoesRoutingModule } from './poes-routing.module';
 import { ListComponent } from './component/list/list.component';
 import { SharedModule } from '../shared/shared.module';
+import { AddComponent } from './component/add/add.component';
 
 @NgModule({
   declarations: [ListComponent, AddComponent],// TODO VIRER ADDComponent
