9a1f89cfbc0922c3cdb5e878e4dc053a35a8148e
Celine <rat.celine@outlook.com>
2023-01-10 10:02:22 +0100

    Provide Factory before application starts

--- src/app/intl/intl.module.ts
+++ src/app/intl/intl.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { intlProvider } from './services/intl.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [
+    intlProvider, // Ajout d'un fournisseur intl dans le module
+  ],
 })
-export class IntlModule { }
+export class IntlModule {}
