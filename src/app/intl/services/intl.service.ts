529f332a9433ef5b07639c414ec02c0f854fd805
Celine <rat.celine@outlook.com>
2023-01-10 09:51:47 +0100

    Add init method and sets the provider

--- src/app/intl/services/intl.service.ts
+++ src/app/intl/services/intl.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { APP_INITIALIZER, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IntlService {
+  constructor() {}
 
-  constructor() { }
+  init(): Promise<void> {
+    return new Promise((resolve: any) => {
+      console.log(`I'm running`);
+      resolve();
+    });
+  }
+
+  public static appInitializer(IntlService: IntlService) {
+    return (): Promise<void> => {
+      return IntlService.init();
+    };
+  }
 }
+
+export const intlProvider = {
+  provide: APP_INITIALIZER, // L'application démarre. Indique à Angular qu'il y a quelque chose à faire via APP_INITIALIZER
+  useFactory: IntlService.appInitializer, // Utilise le service
+  deps: [
+    IntlService, // Permet d'utiliser le service dans la static (instancier la class)
+  ],
+  multi: true, // Parce qu'angular, par défault, n'autorise qu'une seule class à utiliser appInitializer. Multi permet d'utiliser plusieurs services
+};
