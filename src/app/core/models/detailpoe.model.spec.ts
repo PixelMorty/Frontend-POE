607b92c7a3ed1cc7e22d713dffdafc2ba4da802c
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-01-17 09:37:52 +0100

    details models added

---
+++ src/app/core/models/detailpoe.model.spec.ts
@@ -0,0 +1,7 @@
+import { Detailpoe } from './detailpoe.model';
+
+describe('Detailpoe', () => {
+  it('should create an instance', () => {
+    expect(new Detailpoe()).toBeTruthy();
+  });
+});
---
+++ src/app/core/models/detailtrainee.model.spec.ts
@@ -0,0 +1,7 @@
+import { Detailtrainee } from './detailtrainee.model';
+
+describe('Detailtrainee', () => {
+  it('should create an instance', () => {
+    expect(new Detailtrainee()).toBeTruthy();
+  });
+});
