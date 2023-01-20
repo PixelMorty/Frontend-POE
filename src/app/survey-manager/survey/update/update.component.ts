75dd11b7a82684dab6f9b3d1f8309866a197254b
Celine <rat.celine@outlook.com>
2023-01-20 17:29:51 +0100

    ajout dans component survey

--- src/app/survey-manager/survey/update/update.component.ts
+++ src/app/survey-manager/survey/update/update.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Survey } from 'src/app/core/models/survey-models/survey.model';
+import { SurveyService } from 'src/app/core/services/survey-service';
 
+// Page de detail du Survey
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss']
+  styleUrls: ['./update.component.scss'],
 })
 export class UpdateComponent implements OnInit {
+  constructor(private surveyService: SurveyService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    // TODO this.surveyService.change-questions(this.listIds, this.idSurvey)
   }
-
 }
