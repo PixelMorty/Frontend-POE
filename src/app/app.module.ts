d274521b773cf01874e5a819428f62e7e373dae6
Celine <rat.celine@outlook.com>
2023-01-23 19:50:29 +0100

    connect menu to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { StagiairesModule } from './stagiaires/stagiaires.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
 import { MatInputModule } from '@angular/material/input';
 import { MomentDateModule } from '@angular/material-moment-adapter';
-import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
-import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import { MY_DATE_FORMATS } from './shared/formulaire-add-general/my-date-formats';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { ListComponent } from './survey-manager/survey/list/list.component';
 
 import { UpdateComponent } from './survey-manager/survey/update/update.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, UpdateComponent],
+  declarations: [AppComponent, ListComponent, UpdateComponent, MenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
