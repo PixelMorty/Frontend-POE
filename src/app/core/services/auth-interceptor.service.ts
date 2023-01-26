47c0e0b9636885880dcbd830c6e6e3b081c8cc28
Lola Haverlan <haverlan.lola@hotmail.fr>
2023-01-26 10:08:23 +0100

    implementINSECURE

---
+++ src/app/core/services/auth-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log("Interceptor is here with request:", req);
+    //TODO; check if request has to be secured or not
+    const newReq = req.clone({});
+    return next.handle(newReq);
+  }
+}
+
+export const authInterceptor = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptorService,
+  multi: true
+}
